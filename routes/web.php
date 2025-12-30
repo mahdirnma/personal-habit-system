1a7f5e0d49237ac28d53105457044a67306c2aba
Mahdi <rahnamamahdi78@gmail.com>
2025-12-30 08:54:01 +0330

    create login and register panels and also build those processes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login',[AuthController::class,'loginForm'])->name('login.form');
+Route::post('/login',[AuthController::class,'login'])->name('login');
+Route::get('/register',[AuthController::class,'registerForm'])->name('register.form');
+Route::post('/register',[AuthController::class,'register'])->name('register');
+Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+
+Route::get('/',[UserController::class,'index'])->name('dashboard');
